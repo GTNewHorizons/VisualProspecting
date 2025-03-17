9140e1dc5193ff5dcbeca27f7fabeafd1abede54
RecursivePineapple <recursive_pineapple@proton.me>
2025-03-16 21:33:44 -0400

    Fixes for unrelated API breakage

--- src/main/java/com/sinthoras/visualprospecting/database/ServerCache.java
+++ src/main/java/com/sinthoras/visualprospecting/database/ServerCache.java
@@ -14,10 +14,10 @@ import com.sinthoras.visualprospecting.Utils;
 import com.sinthoras.visualprospecting.VP;
 import com.sinthoras.visualprospecting.database.veintypes.VeinType;
 import com.sinthoras.visualprospecting.database.veintypes.VeinTypeCaching;
-import com.sinthoras.visualprospecting.integration.gregtech.UndergroundFluidsWrapper;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import gregtech.api.events.VeinGenerateEvent;
+import gregtech.common.UndergroundOil;
 import gregtech.common.WorldgenGTOreLayer;
 
 public class ServerCache extends WorldCache {
@@ -115,8 +115,8 @@ public class ServerCache extends WorldCache {
                 Fluid fluid = null;
                 for (int offsetChunkX = 0; offsetChunkX < VP.undergroundFluidSizeChunkX; offsetChunkX++) {
                     for (int offsetChunkZ = 0; offsetChunkZ < VP.undergroundFluidSizeChunkZ; offsetChunkZ++) {
-                        final FluidStack prospectedFluid = UndergroundFluidsWrapper
-                                .prospectFluid(world, chunkX + offsetChunkX, chunkZ + offsetChunkZ);
+                        final FluidStack prospectedFluid = UndergroundOil
+                                .undergroundOil(world, chunkX + offsetChunkX, chunkZ + offsetChunkZ, -1);
                         if (prospectedFluid != null) {
                             fluid = prospectedFluid.getFluid();
                             chunks[offsetChunkX][offsetChunkZ] = prospectedFluid.amount;
