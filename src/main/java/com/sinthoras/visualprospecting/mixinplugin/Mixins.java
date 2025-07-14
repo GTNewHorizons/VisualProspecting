ff7427a83c65ba7010ae555a52105da4a04879b6
Alexdoru <57050655+Alexdoru@users.noreply.github.com>
2025-07-14 14:22:29 +0200

    Convert to new IMixins and MixinsBuilder system

--- src/main/java/com/sinthoras/visualprospecting/mixinplugin/Mixins.java
+++ src/main/java/com/sinthoras/visualprospecting/mixinplugin/Mixins.java
@@ -2,14 +2,14 @@ package com.sinthoras.visualprospecting.mixinplugin;
 
 import static com.sinthoras.visualprospecting.mixinplugin.TargetedMod.*;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
+import javax.annotation.Nonnull;
 
-import cpw.mods.fml.relauncher.FMLLaunchHandler;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
+import com.gtnewhorizon.gtnhmixins.builders.MixinBuilder;
 
-public enum Mixins {
+public enum Mixins implements IMixins {
 
+    // spotless:off
     // Bartworks mixins
     WorldGenContainerMixin("bartworks.WorldGenContainerMixin", BARTWORKS),
 
@@ -23,75 +23,26 @@ public enum Mixins {
     GT_WorldGenContainerMixin("gregtech.WorldGenContainerMixin", GT5U),
 
     // Vanilla Mixins
-    MinecraftServerAccessorMixin("minecraft.MinecraftServerAccessor", Phase.EARLY, Side.BOTH, VANILLA),
-    ItemEditableBookMixin("minecraft.ItemEditableBookMixin", Phase.EARLY, VANILLA);
+    MINECRAFT(new MixinBuilder()
+            .addCommonMixins(
+                    "minecraft.MinecraftServerAccessor",
+                    "minecraft.ItemEditableBookMixin")
+            .setPhase(Phase.EARLY));
+    // spotless:on
 
-    public final String mixinClass;
-    public final Phase phase;
-    private final Side side;
-    private final List<TargetedMod> targetedMods;
+    private final MixinBuilder builder;
 
-    Mixins(String mixinClass, TargetedMod... targetedMods) {
-        this.mixinClass = mixinClass;
-        this.phase = Phase.LATE;
-        this.side = Side.BOTH;
-        this.targetedMods = Arrays.asList(targetedMods);
+    Mixins(MixinBuilder builder) {
+        this.builder = builder;
     }
 
-    Mixins(String mixinClass, Phase phase, TargetedMod... targetedMods) {
-        this.mixinClass = mixinClass;
-        this.phase = phase;
-        this.side = Side.BOTH;
-        this.targetedMods = Arrays.asList(targetedMods);
+    Mixins(String mixinClass, TargetedMod requiredMod) {
+        this.builder = new MixinBuilder().addCommonMixins(mixinClass).addRequiredMod(requiredMod).setPhase(Phase.LATE);
     }
 
-    Mixins(String mixinClass, Side side, TargetedMod... targetedMods) {
-        this.mixinClass = mixinClass;
-        this.phase = Phase.LATE;
-        this.side = side;
-        this.targetedMods = Arrays.asList(targetedMods);
+    @Nonnull
+    @Override
+    public MixinBuilder getBuilder() {
+        return this.builder;
     }
-
-    Mixins(String mixinClass, Phase phase, Side side, TargetedMod... targetedMods) {
-        this.mixinClass = mixinClass;
-        this.phase = phase;
-        this.side = side;
-        this.targetedMods = Arrays.asList(targetedMods);
-    }
-
-    public boolean shouldLoad(Set<String> loadedCoreMods, Set<String> loadedMods) {
-        return shouldLoadSide() && allModsLoaded(targetedMods, loadedCoreMods, loadedMods);
-    }
-
-    private boolean shouldLoadSide() {
-        return (side == Side.BOTH || (side == Side.SERVER && FMLLaunchHandler.side().isServer())
-                || (side == Side.CLIENT && FMLLaunchHandler.side().isClient()));
-    }
-
-    private boolean allModsLoaded(List<TargetedMod> targetedMods, Set<String> loadedCoreMods, Set<String> loadedMods) {
-        if (targetedMods.isEmpty()) return false;
-        for (TargetedMod target : targetedMods) {
-            if (target == TargetedMod.VANILLA) continue;
-            // Check coremod first
-            if (!loadedCoreMods.isEmpty() && target.coreModClass != null
-                    && !loadedCoreMods.contains(target.coreModClass)) {
-                return false;
-            } else if (!loadedMods.isEmpty() && target.modId != null && !loadedMods.contains(target.modId)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    enum Side {
-        BOTH,
-        CLIENT,
-        SERVER
-    }
-
-    public enum Phase {
-        EARLY,
-        LATE
-    }
-
 }
--- src/main/java/com/sinthoras/visualprospecting/mixinplugin/VisualProspectingEarlyMixinLoader.java
+++ src/main/java/com/sinthoras/visualprospecting/mixinplugin/VisualProspectingEarlyMixinLoader.java
@@ -1,34 +1,17 @@
 package com.sinthoras.visualprospecting.mixinplugin;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import com.gtnewhorizon.gtnhmixins.IEarlyMixinLoader;
+import com.gtnewhorizon.gtnhmixins.builders.IMixins;
 
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
 
 @IFMLLoadingPlugin.MCVersion("1.7.10")
 @IFMLLoadingPlugin.TransformerExclusions("com.sinthoras.visualprospecting.mixinplugin")
 public class VisualProspectingEarlyMixinLoader implements IFMLLoadingPlugin, IEarlyMixinLoader {
-
-    @Override
-    public String getMixinConfig() {
-        return "mixins.visualprospecting.early.json";
-    }
-
-    @Override
-    public List<String> getMixins(Set<String> loadedCoreMods) {
-        final List<String> mixins = new ArrayList<>();
-        for (Mixins mixin : Mixins.values()) {
-            if (mixin.phase == Mixins.Phase.EARLY && mixin.shouldLoad(loadedCoreMods, Collections.emptySet())) {
-                mixins.add(mixin.mixinClass);
-            }
-        }
-        return mixins;
-    }
 
     @Override
     public String[] getASMTransformerClass() {
@@ -53,4 +36,13 @@ public class VisualProspectingEarlyMixinLoader implements IFMLLoadingPlugin, IEa
         return null;
     }
 
+    @Override
+    public String getMixinConfig() {
+        return "mixins.visualprospecting.early.json";
+    }
+
+    @Override
+    public List<String> getMixins(Set<String> loadedCoreMods) {
+        return IMixins.getEarlyMixins(Mixins.class, loadedCoreMods);
+    }
 }
